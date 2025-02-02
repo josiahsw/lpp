8cee4fa1905c15015155f41b6201e3bb0fd89af2
josiahsw <josiahwaderich@gmail.com>
2025-02-02 14:26:51 -0600

    added tests

--- tests/testthat/test-find_optimal_zscores.R
+++ tests/testthat/test-find_optimal_zscores.R
@@ -30,7 +30,21 @@ test_that("allocate_bench_slots() works when n_bench_drafted is even", {
   expect_equal(slots$pit, pit_pos * teams + n_bench_drafted %/% 2)
 })
 
-test_that("iteration works", {
+test_that("i is greater than 2", {
+  teams <- 12
+  bat_cat = c("HR", "R", "RBI", "SB", "OBP")
+  pit_cat = c("WQS", "SVHLD", "SO", "ERA", "WHIP")
+  bat_pos = c("C" = 1, "1B" = 1, "2B" = 1, "3B" = 1, "SS" = 1, "CI" = 1, 
+              "MI" = 1, "OF" = 5, "UT" = 1)
+  pit_pos = c("SP" = 6, "RP" = 3, "P" = 0)
+  bench = 2 
+  
+  results <- clean_projections(batter_projections, pitcher_projections) %>%
+    find_optimal_zscores(bat_pos, pit_pos, bench, teams, bat_cat, pit_cat, max_i = 25, test = TRUE)
+  expect_true(length(results) > 2)
+})
+
+test_that("iteration doesn't modify rows, columns, or colnames", {
   teams <- 12
   bat_cat = c("HR", "R", "RBI", "SB", "OBP")
   pit_cat = c("WQS", "SVHLD", "SO", "ERA", "WHIP")
@@ -44,4 +58,11 @@ test_that("iteration works", {
   i1 <- results[[1]]
   i2 <- results[[2]]
   
+  expect_equal(nrow(i1$bat), nrow(i2$bat))
+  expect_equal(nrow(i1$pit), nrow(i2$pit))
+  expect_equal(ncol(i1$bat), ncol(i2$bat))
+  expect_equal(ncol(i1$pit), ncol(i2$pit))
+  expect_equal(names(i1$bat), names(i2$bat))
+  expect_equal(names(i1$pit), names(i2$pit))
 })
+
