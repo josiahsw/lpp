a381c96e469b2068caf79c3b7192e4be35bd5e5f
josiahsw <josiahwaderich@gmail.com>
2025-02-02 12:42:35 -0600

    added test funcitonality

--- R/find_optimal_zscores.R
+++ R/find_optimal_zscores.R
@@ -5,12 +5,17 @@
 #' @param cleaned_projections A list of length 2 containing the output of 
 #'                            clean_projections()
 #' @inheritParams lpp
+#' @param max_i An integer, the maximum number of iterations. Defaults to 25.
+#' @param test Select TRUE if testing the function, which will save the results 
+#'             of each iteration for inspection. Defaults to FALSE.
 #'
 #' @returns A list of length 2 containing a data frame of batter and pitcher 
 #'          z-scores.
 #' @importFrom utils head
 #' @noRd 
-find_optimal_zscores <- function(cleaned_projections, bat_pos, pit_pos, bench, teams, bat_cat, pit_cat) {
+find_optimal_zscores <- function(cleaned_projections, bat_pos, pit_pos, bench, 
+                                 teams, bat_cat, pit_cat, 
+                                 max_i = 25, test = FALSE) {
   stopifnot(
     all(names(cleaned_projections) %in% c("bat", "pit")), 
     length(cleaned_projections) == 2
@@ -24,14 +29,15 @@ find_optimal_zscores <- function(cleaned_projections, bat_pos, pit_pos, bench, t
     pit = pit_pos * teams,
     bench = c(bench = bench * teams)
     )
-  
   # variables needed for while loop
   i <- 0
   prior_rank <- NULL
   current_rank <-
     c(utils::head(cleaned_projections$bat$fangraphs_id, n_drafted$bat,
       utils::head(cleaned_projections$pit$fangraphs_id, n_drafted$pit)))
-  max_iterations <- 25
+  max_iterations <- max_i
+  # only used for testing
+  test_list <- list()
   
   while (i < max_iterations && !identical(current_rank, prior_rank)) {
     prior_rank <- current_rank
@@ -53,51 +59,60 @@ find_optimal_zscores <- function(cleaned_projections, bat_pos, pit_pos, bench, t
     
     current_rank <- combined_rankings(bat, pit)
     i <- i + 1
+    
+    if (test == TRUE) {
+      test_list[[i]] <- list(bat = bat, pit = pit)
+    }
   }
   
-  if (i >= max_iterations) {
-    message("Max iterations reached without stable rankings.")
+  if (test == TRUE) {
+    return(test_list)
   } else {
-    message("Stable rankings reached after ", i, " iterations.")
+    
+    if (i >= max_iterations) {
+      message("Max iterations reached without stable rankings.")
+    } else {
+      message("Stable rankings reached after ", i, " iterations.")
+    }
+    
+    # clean draft results
+    zbat <- paste0("z", bat_cat)
+    zpit <- paste0("z", pit_cat)
+    
+    bat <- bat %>%
+      dplyr::select(
+        fangraphs_id,
+        player_name,
+        minpos,
+        pos,
+        ADP,
+        PA,
+        dplyr::all_of(zbat),
+        zSUM,
+        drafted
+      ) %>%
+      dplyr::arrange(
+        dplyr::desc(zSUM)
+      )
+    
+    pit <- pit %>%
+      dplyr::select(
+        fangraphs_id,
+        player_name,
+        pos,
+        ADP,
+        IP,
+        dplyr::all_of(zpit),
+        zSUM,
+        drafted
+      ) %>%
+      dplyr::arrange(
+        dplyr::desc(zSUM)
+      )
+    
+    results <- list(bat = bat, pit = pit)
+    return(results)
   }
-  
-  # clean draft results
-  zbat <- paste0("z", bat_cat)
-  zpit <- paste0("z", pit_cat)
-  
-  bat <- bat %>%
-    dplyr::select(
-      fangraphs_id,
-      player_name,
-      minpos,
-      pos,
-      ADP,
-      PA,
-      dplyr::all_of(zbat),
-      zSUM,
-      drafted
-    ) %>%
-    dplyr::arrange(
-      dplyr::desc(zSUM)
-    )
-  
-  pit <- pit %>%
-    dplyr::select(
-      fangraphs_id,
-      player_name,
-      pos,
-      ADP,
-      IP,
-      dplyr::all_of(zpit),
-      zSUM,
-      drafted
-    ) %>%
-    dplyr::arrange(
-      dplyr::desc(zSUM)
-    )
-  
-  results <- list(bat = bat, pit = pit)
-  return(results)
 }
 
 # helpers -----------------------------------------------------------------
