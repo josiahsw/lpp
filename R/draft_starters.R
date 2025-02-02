d44af7baaa50a410686b2a1ec54bbeb7f2b10347
josiahsw <josiahwaderich@gmail.com>
2025-02-02 10:16:37 -0600

    updated documentation

--- R/draft_starters.R
+++ R/draft_starters.R
@@ -11,7 +11,7 @@
 #'                         drafted at each position. 
 #' @inheritParams weight_rate_stats
 #' 
-#' @return The zscore_projection data frame with updated drafted column
+#' @return The zscore_projection data frame with updated drafted column.
 #' @noRd
 draft_starters <- function(zscore_projection, n_drafted_by_pos, stat) {
   stat <- match.arg(stat, choices = c("bat", "pit"))
@@ -27,22 +27,18 @@ draft_starters <- function(zscore_projection, n_drafted_by_pos, stat) {
             names(n_drafted_by_pos)))
     
     main_slots <- n_drafted_by_pos[c("C", "1B", "2B", "3B", "SS", "OF")]
-    
     drafted <- vector("list", length(main_slots))
     for (i in seq_along(main_slots)) {
       drafted[[i]] <- find_top_avail(zscore_projection, main_slots[i])
     }
-    
     main <- unlist(drafted)
     draft_results <- mark_drafted_players(zscore_projection, main)
     
     multi_slots <- n_drafted_by_pos[c("CI", "MI")]
-    
     drafted <- vector("list", length(multi_slots))
     for (i in seq_along(multi_slots)) {
       drafted[[i]] <- find_top_avail(draft_results, multi_slots[i])
     }
-    
     multi <- unlist(drafted)
     draft_results <- mark_drafted_players(draft_results, multi)
     
@@ -52,12 +48,10 @@ draft_starters <- function(zscore_projection, n_drafted_by_pos, stat) {
     stopifnot(all(c("SP", "RP", "P") %in% names(n_drafted_by_pos)))
     
     main_slots <- n_drafted_by_pos[c("SP", "RP")]
-    
     drafted <- vector("list", length(main_slots))
     for (i in seq_along(main_slots)) {
       drafted[[i]] <- find_top_avail(zscore_projection, main_slots[i])
     }
-    
     main <- unlist(drafted)
     draft_results <- mark_drafted_players(zscore_projection, main)
     
@@ -74,11 +68,12 @@ draft_starters <- function(zscore_projection, n_drafted_by_pos, stat) {
 #' z-score. For example, for the slot "CI = 12" it will return a vector
 #' of player ids of the top 12 un-drafted players eligible for the CI slot. 
 #'
-#' @param df A data frame of player projections
+#' @param df A data frame of player projections with currently drafted/undrafted
+#'          players identified.
 #' @param slot A named vector, where the name is the slot and the value is the 
 #'    number of players drafted at that slot.
 #'
-#' @return A character vector of fangraphs_id of the top available players at a 
+#' @return A character vector of fangraphs_ids of the top available players at a 
 #'    slot.
 #' @noRd
 find_top_avail <- function(df, slot) {
@@ -99,29 +94,33 @@ find_top_avail <- function(df, slot) {
     df <- subset(df, !drafted & pos == names(slot))
   }
   
-  fg_ids <- head(df$fangraphs_id, n = slot)
-  return(fg_ids)
+  top_avail <- head(df$fangraphs_id, n = slot)
+  return(top_avail)
 }
 
 #' Mark drafted players
 #'
-#' @param df A data frame of player projections
-#' @param drafted_players Character vector, fangraphs_ids of drafted players
+#' @inheritParams find_top_avail
+#' @param top_avail A character vector of fangraphs_ids of the top available 
+#'                  players at a slot. The output of find_top_avail().
 #'
-#' @return A data frame with drafted players marked T
+#' @return The data frame with top available players marked as drafted = TRUE.
 #' @noRd
-mark_drafted_players <- function(df, drafted_players) {
-  df$drafted <- ifelse(df$fangraphs_id %in% drafted_players, T, df$drafted)
+mark_drafted_players <- function(df, top_avail) {
+  df$drafted <- ifelse(df$fangraphs_id %in% top_avail, T, df$drafted)
   return(df)
 }
 
 #' Identify top available bench players
+#' 
+#' Combines batter and pitcher data frames to find top remaining un-drafted 
+#' players.
 #'
-#' @param df_bat Data frame of drafted batters
-#' @param df_pit Data frame of drafted pitchers
-#' @param bench_slots A named vector of bench slots
+#' @param df_bat A data frame of drafted starting batters.
+#' @param df_pit A data frame of drafted starting pitchers.
+#' @param bench_slots A named vector, the number of bench players drafted.
 #'
-#' @return A character vector of fangraphs_ids for top available bench players
+#' @return A character vector of fangraphs_ids for top available bench players.
 #' @noRd
 top_avail_bench <- function(df_bat, df_pit, bench_slots) {
   if (bench_slots == 0) {
@@ -134,16 +133,26 @@ top_avail_bench <- function(df_bat, df_pit, bench_slots) {
   all <- all[order(all$zSUM, decreasing = TRUE), ]
   
   bench <- find_top_avail(all, bench_slots)
-  
   return(bench)
 } 
 
-combined_rankings <- function(bat, pit) {
-  b <- bat %>%
+#' Find the final ranking of all players (batters and pitchers combined)
+#'
+#' @param drafted_bat A data frame with drafted starting and bench batters 
+#'                    identified.
+#' @param drafted_pit A data frame with drafted starting and bench pitchers 
+#'                    identified.
+#'
+#' @returns A character vector, rank ordered fangraphs_ids of drafted players.
+#' @export
+#'
+#' @examples
+combined_rankings <- function(drafted_bat, drafted_pit) {
+  b <- drafted_bat %>%
     dplyr::filter(drafted == TRUE) %>%
     dplyr::select(fangraphs_id, zSUM)
   
-  p <- pit %>%
+  p <- drafted_pit %>%
     dplyr::filter(drafted == TRUE) %>%
     dplyr::select(fangraphs_id, zSUM)
   
@@ -153,4 +162,3 @@ combined_rankings <- function(bat, pit) {
   ranks <- all$fangraphs_id
   return(ranks)
 }
-
