0e6966f1c0474169572b4da8281edef5aa98e241
josiahsw <josiahwaderich@gmail.com>
2025-02-05 21:24:56 -0600

    added tests for new functions

--- tests/testthat/test-position_adjustment.R
+++ tests/testthat/test-position_adjustment.R
@@ -1,4 +1,105 @@
-test_that("bat_pit pos_adj method works", {
+test_that("find_n_drafted() works", {
+  teams <- 12
+  bat_cat = c("HR", "R", "RBI", "SB", "OBP")
+  pit_cat = c("WQS", "SVHLD", "SO", "ERA", "WHIP")
+  bat_pos = c("C" = 1, "1B" = 1, "2B" = 1, "3B" = 1, "SS" = 1, "CI" = 1, 
+              "MI" = 1, "OF" = 5, "UT" = 1)
+  pit_pos = c("SP" = 6, "RP" = 3, "P" = 0)
+  bench = 2 
+  ctrl <- clean_projections(batter_projections, pitcher_projections) %>%
+    find_optimal_zscores(bat_pos, pit_pos, bench, teams, bat_cat, pit_cat)
+  
+  ctrl_bat <- sum(ctrl$bat$drafted == TRUE)
+  ctrl_pit <- sum(ctrl$pit$drafted == TRUE)
+  test <- lapply(ctrl, find_n_drafted)
+  
+  expect_equal(ctrl_bat, test$bat)
+  expect_equal(ctrl_pit, test$pit)
+})
+
+test_that("combined_lpp_zscore() works", {
+  teams <- 12
+  bat_cat = c("HR", "R", "RBI", "SB", "OBP")
+  pit_cat = c("WQS", "SVHLD", "SO", "ERA", "WHIP")
+  bat_pos = c("C" = 1, "1B" = 1, "2B" = 1, "3B" = 1, "SS" = 1, "CI" = 1, 
+              "MI" = 1, "OF" = 5, "UT" = 1)
+  pit_pos = c("SP" = 6, "RP" = 3, "P" = 0)
+  bench = 2 
+  ctrl <- clean_projections(batter_projections, pitcher_projections) %>%
+    find_optimal_zscores(bat_pos, pit_pos, bench, teams, bat_cat, pit_cat)
+  
+  n_drafted <- lapply(ctrl, find_n_drafted)
+  n_drafted <- sum(unlist(n_drafted))
+  ctrl_zSUM <- sort(c(ctrl$bat$zSUM, ctrl$pit$zSUM), decreasing = TRUE)
+  ctrl_zlpp <- ctrl_zSUM[n_drafted]
+  test_zlpp <- combined_lpp_zscore(ctrl, n_drafted)
+  
+  # find the correct z-score
+  expect_equal(ctrl_zlpp, test_zlpp)
+})
+
+test_that("add_pos_adj() works", {
+  teams <- 12
+  bat_cat = c("HR", "R", "RBI", "SB", "OBP")
+  pit_cat = c("WQS", "SVHLD", "SO", "ERA", "WHIP")
+  bat_pos = c("C" = 1, "1B" = 1, "2B" = 1, "3B" = 1, "SS" = 1, "CI" = 1, 
+              "MI" = 1, "OF" = 5, "UT" = 1)
+  pit_pos = c("SP" = 6, "RP" = 3, "P" = 0)
+  bench = 2 
+  ctrl <- clean_projections(batter_projections, pitcher_projections) %>%
+    find_optimal_zscores(bat_pos, pit_pos, bench, teams, bat_cat, pit_cat)
+  
+  expect_false(all(c("aPOS", "aSUM") %in% names(ctrl$bat)))
+  
+  zlpp <- -2
+  test <- add_pos_adj(ctrl$bat, zlpp)
+  
+  # correct columns are created
+  expect_true(all(c("aPOS", "aSUM") %in% names(test)))
+  
+  # aPOS values are correct
+  expect_true(all(test$aPOS == zlpp))
+  
+  # aSUM calculates correctly
+  expect_equal(test$aSUM, test$zSUM - test$aPOS)
+})
+
+test_that("pos_adj == none works", {
+  teams <- 12
+  bat_cat = c("HR", "R", "RBI", "SB", "OBP")
+  pit_cat = c("WQS", "SVHLD", "SO", "ERA", "WHIP")
+  bat_pos = c("C" = 1, "1B" = 1, "2B" = 1, "3B" = 1, "SS" = 1, "CI" = 1, 
+              "MI" = 1, "OF" = 5, "UT" = 1)
+  pit_pos = c("SP" = 6, "RP" = 3, "P" = 0)
+  bench = 2 
+  ctrl <- clean_projections(batter_projections, pitcher_projections) %>%
+    find_optimal_zscores(bat_pos, pit_pos, bench, teams, bat_cat, pit_cat)
+  test <- clean_projections(batter_projections, pitcher_projections) %>%
+    find_optimal_zscores(bat_pos, pit_pos, bench, teams, bat_cat, pit_cat) %>%
+    position_adjustment(pos_adj = "none")
+  
+  n_drafted <- lapply(ctrl, find_n_drafted)
+  n_drafted <- sum(unlist(n_drafted))
+  zlpp <- combined_lpp_zscore(ctrl, n_drafted)
+  ctrl <- lapply(ctrl, add_pos_adj, zlpp = zlpp)
+  
+  # output is expected
+  expect_true(length(test) == 2)
+  expect_true(all(names(test) %in% c("bat", "pit"))) 
+  
+  # all batter and pitcher position adjustments are the same
+  expect_true(setequal(test$bat$aPOS, test$pit$aPOS))
+  
+  # the number of players drafted == the number of players where aSUM >= 0
+  b <- sum(test$bat$aSUM >= 0)
+  p <- sum(test$pit$aSUM >= 0)
+  expect_equal(n_drafted, b + p)
+  
+  # test output is the same as control output
+  expect_identical(ctrl, test)
+})
+
+test_that("pos_adj == bat_pit works", {
   teams <- 12
   bat_cat = c("HR", "R", "RBI", "SB", "OBP")
   pit_cat = c("WQS", "SVHLD", "SO", "ERA", "WHIP")
@@ -12,6 +113,11 @@ test_that("bat_pit pos_adj method works", {
   test <- clean_projections(batter_projections, pitcher_projections) %>%
     find_optimal_zscores(bat_pos, pit_pos, bench, teams, bat_cat, pit_cat) %>%
     position_adjustment(pos_adj = "bat_pit")
+  
+  n_drafted <- lapply(ctrl, find_n_drafted)
+  df <- df[order(-df$zSUM), ] # ensures df is sorted
+  zlpp <- df$zSUM[n_drafted] # last player picked z-score
+  add_pos_adj(df, zlpp)
   
   # number of pitchers and batters drafted is the same in control and test
   expect_equal(sum(ctrl$bat$drafted == TRUE), sum(test$bat$aSUM >= 0))
