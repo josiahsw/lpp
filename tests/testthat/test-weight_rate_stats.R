6a7d61a63b5fa20d11f869540d861a6d12944224
josiahsw <josiahwaderich@gmail.com>
2025-02-08 10:35:20 -0600

    revised tests, removed tests of simple helpers

--- tests/testthat/test-weight_rate_stats.R
+++ tests/testthat/test-weight_rate_stats.R
@@ -1,69 +1,34 @@
-test_that("weight_rate_stats() assigns drafted values if missing", {
-  # check test data
+test_that("drafted values are assigned if missing", {
   result <- clean_projections(batter_projections, pitcher_projections)
   expect_true(!all(result$bat$drafted))
+  expect_true(!all(result$pit$drafted))
   
   n_drafted <- 150
-  df <- weight_rate_stats(result$bat, n_drafted, "bat")
-  expect_equal(sum(df$drafted), n_drafted)
+  bat <- weight_rate_stats(result$bat, n_drafted, "bat")
+  pit <- weight_rate_stats(result$pit, n_drafted, "pit")
+  expect_equal(sum(bat$drafted), n_drafted)
+  expect_equal(sum(pit$drafted), n_drafted)
 })
 
-test_that("weight_rate_stats() does not assign drafted values if they exist", {
-  # check test data
+test_that("drafted values are not assigned if they exist", {
   result <- clean_projections(batter_projections, pitcher_projections)
   result$bat$drafted[1] <- TRUE
+  result$pit$drafted[1] <- TRUE
   expect_equal(sum(result$bat$drafted), 1)
+  expect_equal(sum(result$pit$drafted), 1)
   
   n_drafted <- 150
-  df <- weight_rate_stats(result$bat, n_drafted, "bat")
-  expect_equal(sum(df$drafted), 1)
+  bat <- weight_rate_stats(result$bat, n_drafted, "bat")
+  pit <- weight_rate_stats(result$pit, n_drafted, "pit")
+  expect_equal(sum(bat$drafted), 1)
+  expect_equal(sum(pit$drafted), 1)
 })
 
-test_that("weight_rate_stats() creates expected columns", {
+test_that("expected columns are created", {
   result <- clean_projections(batter_projections, pitcher_projections)
   n_drafted <- 150
   wbat <- weight_rate_stats(result$bat, n_drafted, "bat")
   wpit <- weight_rate_stats(result$pit, n_drafted, "pit")
   expect_true(all(c("wAVG", "wOBP") %in% names(wbat)))
   expect_true(all(c("wERA", "wWHIP") %in% names(wpit)))
-})
-
-test_that("draftpool_summary() throws error if no drafted players", {
-  # check test data
-  result <- clean_projections(batter_projections, pitcher_projections)
-  expect_true(!all(result$bat$drafted))
-  
-  expect_error(draftpool_summary(result$bat, mean))
-})
-
-test_that("draftpool_summary() returns numeric vector", {
-  result <- clean_projections(batter_projections, pitcher_projections)
-  result$bat$drafted[1:150] <- TRUE
-  
-  dp_mean <- draftpool_summary(result$bat, mean)
-  expect_true(is.vector(dp_mean))
-  expect_true(is.numeric(dp_mean))
-})
-
-test_that("draftpool_summary() works for mean and sd", {
-  result <- clean_projections(batter_projections, pitcher_projections)
-  result$bat$drafted[1:150] <- TRUE
-  
-  test_mean <- mean(result$bat$HR[result$bat$drafted == TRUE])
-  test_sd <- sd(result$bat$HR[result$bat$drafted == TRUE])
-  
-  dp_mean <- draftpool_summary(result$bat, mean)[["HR"]]
-  dp_sd <- draftpool_summary(result$bat, sd)[["HR"]]
-  
-  expect_equal(dp_mean, test_mean)
-  expect_equal(dp_sd, test_sd)
-})
-
-test_that("x_above_avg() works", {
-  x <- 12
-  pt <- 40
-  dp_mean <- .25
-  
-  result <- x_above_avg(x, pt, dp_mean)
-  expect_equal(result, 2)
 })
