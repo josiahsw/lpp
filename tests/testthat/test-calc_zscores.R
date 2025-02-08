a45d2ea36924b2ed439ff9d1b8f1bd41d637e65f
josiahsw <josiahwaderich@gmail.com>
2025-02-08 13:12:13 -0600

    updated tests

--- tests/testthat/test-calc_zscores.R
+++ tests/testthat/test-calc_zscores.R
@@ -1,26 +1,58 @@
-test_that("calc_zscores() returns accurate zSUM", {
+test_that("batter zSUM is accurate", {
+  cleaned <- clean_projections(batter_projections, pitcher_projections)
   categories <- c("HR", "R", "RBI", "SB", "AVG", "OBP")
   bat_cat <- sample(categories, 5, replace = FALSE) # random selection for test
   z_cols <- paste0("z", bat_cat)
-  results <- clean_projections(batter_projections, pitcher_projections)
   n_drafted <- 100
   
-  df <- weight_rate_stats(results$bat, n_drafted, "bat") %>%
-    calc_zscores(bat_cat, "bat")
+  z <- weight_rate_stats(results$bat, n_drafted) |>
+    calc_zscores(bat_cat)
   
-  df1 <- df %>%
-    dplyr::rowwise() %>%
-    dplyr::mutate(zSUM1 = sum(dplyr::across(dplyr::all_of(z_cols)), na.rm = TRUE)) %>%
+  test <- z |>
+    dplyr::rowwise() |>
+    dplyr::mutate(zSUM1 = sum(dplyr::across(dplyr::all_of(z_cols)), na.rm = TRUE)) |>
     dplyr::ungroup()
   
-  expect_true("zSUM" %in% colnames(df))
-  expect_equal(df$zSUM, df1$zSUM1)
+  expect_equal(z$zSUM, test$zSUM1)
 })
 
-test_that("z_score() works", {
-  raw_score <- 5
-  pop_mean <- 3
-  pop_sd <- 2
-  result <- z_score(raw_score, pop_mean, pop_sd)
-  expect_equal(result, 1)
+test_that("pitcher zSUM is accurate", {
+  cleaned <- clean_projections(batter_projections, pitcher_projections)
+  categories <- c("WQS", "SVHLD", "SO", "ERA", "WHIP", "W")
+  pit_cat <- sample(categories, 5, replace = FALSE) # random selection for test
+  z_cols <- paste0("z", pit_cat)
+  n_drafted <- 100
+  
+  z <- weight_rate_stats(cleaned$pit, n_drafted) |>
+    calc_zscores(pit_cat)
+  
+  test <- z |>
+    dplyr::rowwise() |>
+    dplyr::mutate(zSUM1 = sum(dplyr::across(dplyr::all_of(z_cols)), na.rm = TRUE)) |>
+    dplyr::ungroup()
+  
+  expect_equal(z$zSUM, test$zSUM1)
+})
+
+test_that("expected batter columns are created", {
+  cleaned <- clean_projections(batter_projections, pitcher_projections)
+  n_drafted <- 100
+  bat_cat <- c("HR", "R", "RBI", "SB", "OBP")
+  z_cols <- c("zHR", "zR", "zRBI", "zSB", "zAVG", "zOBP", "zSUM")
+  z <- weight_rate_stats(cleaned$bat, n_drafted) |>
+    calc_zscores(bat_cat)
+  
+  expect_true(all(z_cols %in% names(z)))
+})
+
+test_that("expected pitcher columns are created", {
+  cleaned <- clean_projections(batter_projections, pitcher_projections)
+  n_drafted <- 100
+  pit_cat <- c("WQS", "SVHLD", "SO", "ERA", "WHIP")
+  z_cols <- c("zW", "zQS", "zWQS", "zSV", "zHLD", "zSVHLD", "zSO", "zERA", 
+              "zWHIP", "zSUM")
+  z <- weight_rate_stats(cleaned$pit, n_drafted) |>
+    calc_zscores(pit_cat)
+  
+  expect_true(all(z_cols %in% names(z)))
 })
