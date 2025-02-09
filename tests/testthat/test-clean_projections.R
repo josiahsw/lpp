52fda1516674dd35be2dbd2b0460d3ce379a2763
josiahsw <josiahwaderich@gmail.com>
2025-02-09 16:02:39 -0600

    added test

--- tests/testthat/test-clean_projections.R
+++ tests/testthat/test-clean_projections.R
@@ -24,3 +24,10 @@ test_that("position priority is correct", {
   pos <- unlist(lapply(minpos, find_priority_pos))
   expect_equal(results$bat$pos, pos)
 })
+
+test_that("object returned with expected structure", {
+  results <- clean_projections(batter_projections, pitcher_projections)
+  expect_true(length(results) == 2)
+  expect_true(all(c("bat", "pit") %in% names(results)))
+  expect_true(all(sapply(results, is.data.frame)))
+})
