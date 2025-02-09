890d64931e71b64f29b33360c3cef85cd27f9c12
josiahsw <josiahwaderich@gmail.com>
2025-02-09 15:56:41 -0600

    revised test file

--- tests/testthat/test-clean_projections.R
+++ tests/testthat/test-clean_projections.R
@@ -18,26 +18,9 @@ test_that("QS col is created if missing", {
   expect_true("QS" %in% colnames(results$pit))
 })
 
-test_that("find_priority_pos() returns single positions", {
-  expect_equal(find_priority_pos("C"), "C")
-  expect_equal(find_priority_pos("SS"), "SS")
-  expect_equal(find_priority_pos("2B"), "2B")
-  expect_equal(find_priority_pos("3B"), "3B")
-  expect_equal(find_priority_pos("DH"), "DH")
-})
-
-test_that("find_priority_pos() returns the priority position", {
-  expect_equal(find_priority_pos("C/OF"), "C")
-  expect_equal(find_priority_pos("3B/SS/DH"), "SS")
-  expect_equal(find_priority_pos("2B/3B"), "2B")
-  expect_equal(find_priority_pos("3B/OF"), "3B")
-  expect_equal(find_priority_pos("1B/OF"), "OF")
-  expect_equal(find_priority_pos("DH/1B"), "1B")
-})
-
-test_that("find_priority_pos() can work with vectorized operations", {
-  minpos <- c("C/OF", "3B/SS/DH", "2B/3B", "3B/OF", "1B/OF", "DH/1B")
-  expected <- c("C", "SS", "2B", "3B", "OF", "1B")
-  result <- purrr::map_chr(minpos, find_priority_pos)
-  expect_equal(result, expected)
+test_that("position priority is correct", {
+  results <- clean_projections(batter_projections, pitcher_projections)
+  minpos <- results$bat$minpos
+  pos <- unlist(lapply(minpos, find_priority_pos))
+  expect_equal(results$bat$pos, pos)
 })
