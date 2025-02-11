2f62e08d14a3e3c58859bbb3de92bd6e97bb0666
josiahsw <josiahwaderich@gmail.com>
2025-02-11 07:25:01 -0600

    added test data

--- tests/testthat/test-clean_projections.R
+++ tests/testthat/test-clean_projections.R
@@ -1,20 +1,23 @@
 test_that("cleaned batter projections include needed columns", {
-  results <- clean_projections(batter_projections, pitcher_projections)
+  test_data <- projection_test_data()
+  results <- clean_projections(test_data$bat, test_data$pit)
   expect_true(
     all(c("fangraphs_id", "player_name", "OB", "pos", "drafted") %in% 
       colnames(results$bat)))
 })
 
 test_that("cleaned pitcher projections include needed columns", {
-  results <- clean_projections(batter_projections, pitcher_projections)
+  test_data <- projection_test_data()
+  results <- clean_projections(test_data$bat, test_data$pit)
   expect_true(
     all(c("fangraphs_id", "player_name", "ER9", "WH", "SVHLD", "pos", "drafted",
           "WQS") %in% colnames(results$pit))) # QS tested in separate test
 })
 
 test_that("QS col is created if missing", {
-  expect_false("QS" %in% colnames(pitcher_projections)) # validate test data
-  results <- clean_projections(batter_projections, pitcher_projections)
+  test_data <- projection_test_data()
+  expect_false("QS" %in% colnames(test_data$pit)) # validate test data
+  results <- clean_projections(test_data$bat, test_data$pit)
   GS <- results$pit$GS
   ERA <- results$pit$ERA
   IP <- results$pit$IP
@@ -25,14 +28,16 @@ test_that("QS col is created if missing", {
 })
 
 test_that("position priority is correct", {
-  results <- clean_projections(batter_projections, pitcher_projections)
+  test_data <- projection_test_data()
+  results <- clean_projections(test_data$bat, test_data$pit)
   minpos <- results$bat$minpos
   pos <- unlist(lapply(minpos, find_priority_pos))
   expect_equal(results$bat$pos, pos)
 })
 
 test_that("object returned with expected structure", {
-  results <- clean_projections(batter_projections, pitcher_projections)
+  test_data <- projection_test_data()
+  results <- clean_projections(test_data$bat, test_data$pit)
   expect_true(length(results) == 2)
   expect_true(all(c("bat", "pit") %in% names(results)))
   expect_true(all(sapply(results, is.data.frame)))
