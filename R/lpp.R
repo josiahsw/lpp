25e820637989e718fd6adf6bab648eddceabc123
josiahsw <josiahwaderich@gmail.com>
2025-02-14 16:14:03 -0600

    updated documentation, changed defaults, added more informative error messsages

--- R/lpp.R
+++ R/lpp.R
@@ -3,21 +3,37 @@
 #' @param bat A data frame of batter projections from fangraphs.
 #' @param pit A data frame of pitcher projections from fangraphs.
 #' @param keepers A data frame of keepers and values. Defaults to NULL.
-#' @param lg A string. "AL", "NL", or "MLB" (the default).
-#' @param teams An integer. The number of teams in the league. Defaults to 12.
-#' @param budget An integer. The starting auction budget of each team in the 
-#'               league. Defaults to 260.
-#' @param min_bid An integer. The minimum auction bid. Defaults to 1.
-#' @param bat_cat A character vector of league batting categories.
-#' @param pit_cat A character vector of league pitching categories.
-#' @param bat_pos A named integer vector. The number of batters drafted at each 
-#'                position per team.
-#' @param pit_pos A named integer vector. The number of pitchers drafted at each 
-#'                position per team. 
-#' @param bench An integer. The number of bench players drafted by each team.
-#' @param pos_adj A string. The position adjustment method. One of "simple", 
-#'                "hold_harmless" (the default), "zero_out", "DH_to_1B", 
-#'                "bat_pit", or "none".
+#' @param lg A string. The league type. One of "MLB" (the default), "AL", or 
+#'           "NL".
+#' @param teams A whole number greater than 0. The number of teams in the 
+#'              league. Defaults to 12.
+#' @param budget A numeric value greater than 0. The starting auction budget of 
+#'               each team in the league. Defaults to 260.
+#' @param min_bid A numeric value greater than or equal to 0. The minimum 
+#'                bid per player. Defaults to 1.
+#' @param bat_cat A character vector of league batting stat categories.
+#'                Available options are HR, R, RBI, SB, AVG, and OBP. The
+#'                default is c("HR", "R", "RBI", "SB", "AVG").
+#' @param pit_cat A character vector of league pitching stat categories. 
+#'                Available options are W, QS, WQS, SV, HLD, SVHLD, SO, ERA,
+#'                and WHIP. If quality starts are not included in the pitcher
+#'                projections expected QS will be automatically calculated based
+#'                on the Cup of Fantasy Joe xQS formula. The default is c("W", 
+#'                "SV", "SO", "ERA", "WHIP").
+#' @param bat_pos A named numeric vector where batting position is the name and 
+#'                the number of batters drafted at that position per team is the 
+#'                value. The default is c("C" = 1, "1B" = 1, "2B" = 1, "3B" = 1, 
+#'                "SS" = 1, "CI" = 1, "MI" = 1, "OF" = 5, "UT" = 1). All 
+#'                position names in the default vector must be given and all 
+#'                values must be greater than or equal to 0. 
+#' @param pit_pos A named numeric vector where pitching position is the name and 
+#'                the number of pitchers drafted at each position per team is 
+#'                the value. The default is c("SP" = 5, "RP" = 3, "P" = 0). All 
+#'                position names in the default vector must be given and all 
+#'                values must be greater than or equal to 0.
+#' @param bench A numeric value, the number of bench players drafted by each 
+#'              team. Defaults to 2. 
+#' @param pos_adj A string, the position adjustment method.
 #'
 #' @returns A list of length 2 containing a data frame of batter and pitcher 
 #'          auction values.
@@ -30,13 +46,13 @@ lpp <- function(
     teams = 12, 
     budget = 260, 
     min_bid = 1, 
-    bat_cat = c("HR", "R", "RBI", "SB", "OBP"),
-    pit_cat = c("WQS", "SVHLD", "SO", "ERA", "WHIP"),
+    bat_cat = c("HR", "R", "RBI", "SB", "AVG"),
+    pit_cat = c("W", "SV", "SO", "ERA", "WHIP"),
     bat_pos = c("C" = 1, "1B" = 1, "2B" = 1, "3B" = 1, "SS" = 1, "CI" = 1, 
                 "MI" = 1, "OF" = 5, "UT" = 1),
-    pit_pos = c("SP" = 6, "RP" = 3, "P" = 0), 
+    pit_pos = c("SP" = 5, "RP" = 3, "P" = 0), 
     bench = 2, 
-    pos_adj = c("hold_harmless", "zero_out", "DH_to_1B","simple", "bat_pit",
+    pos_adj = c("hold_harmless", "zero_out", "DH_to_1B", "simple", "bat_pit",
                 "none")
     ) {
   
@@ -46,17 +62,22 @@ lpp <- function(
   stopifnot(
     !is.null(bat),
     !is.null(pit),
-    !is.null(teams), teams > 0, teams %% 1 == 0, # a whole number
+    !is.null(teams), teams > 0, 
+    "'teams' must be a whole number" = teams %% 1 == 0,
     !is.null(budget), budget > 0,
     !is.null(min_bid), min_bid >= 0,
     !is.null(bat_cat),
-    all(bat_cat %in% c("HR", "R", "RBI", "SB", "AVG", "OBP")),
+    "all elements of 'bat_cat' must be one of the available options. See ?lpp for more detail." = 
+      all(bat_cat %in% c("HR", "R", "RBI", "SB", "AVG", "OBP")),
     !is.null(pit_cat),
-    all(pit_cat %in% c("W", "QS", "WQS", "SV", "HLD", "SVHLD", "SO", "ERA", "WHIP")),
+    "all elements of 'pit_cat' must be one of the available options. See ?lpp for more detail." = 
+      all(pit_cat %in% c("W", "QS", "WQS", "SV", "HLD", "SVHLD", "SO", "ERA", "WHIP")),
     !is.null(bat_pos), all(bat_pos >= 0),
-    setequal(names(bat_pos), c("C", "1B", "2B", "3B", "SS", "CI", "MI", "OF", "UT")),
+    "'bat_pos' must include all default batter positions. See ?lpp for more detail." = 
+      setequal(names(bat_pos), c("C", "1B", "2B", "3B", "SS", "CI", "MI", "OF", "UT")),
     !is.null(pit_pos), all(pit_pos >= 0),
-    setequal(names(pit_pos), c("SP", "RP", "P")),
+    "'pit_pos' must include all default pitcher positions. See ?lpp for more detail." = 
+     setequal(names(pit_pos), c("SP", "RP", "P")),
     !is.null(bench), bench >= 0
   )
   
